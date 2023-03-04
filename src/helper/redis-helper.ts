5a42e82d4fe53490baaf7ac82d48025e29cb27b9
Gökhan Uysal <guysal20@ku.edu.tr>
2023-03-04 23:46:27 +0300

    test done

--- src/helper/redis-helper.ts
+++ src/helper/redis-helper.ts
@@ -1,17 +1,17 @@
-import { RedisClientType, createClient } from 'redis';
+import { RedisClientType } from 'redis';
 
 export class RedisHelper {
     private redisClient: RedisClientType;
 
     public constructor(redisClient: RedisClientType) {
-        this.redisClient = createClient();
+        this.redisClient = redisClient;
     }
 
     async setChache(key: any, value: any) {
         await this.redisClient.SET(key, value);
     }
 
-    async getCache(key: any) {
+    async getCache(key: any): Promise<any> {
         return await this.redisClient.GET(key);
     }
 
