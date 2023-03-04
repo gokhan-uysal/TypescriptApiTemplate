f4baa7f0bec2b4d751d111a14d0803a5d2d8e06c
Gökhan Uysal <guysal20@ku.edu.tr>
2023-03-04 13:57:08 +0300

    Mongo and redis config added

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,27 @@
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
 
 dotenv.config();
 
+//Mongo config
 const MONGO_USER: string = process.env.MONGO_USER || '';
 const MONGO_PASSWORD: string = process.env.MONGO_PASSWORD || '';
-const url: string = `${MONGO_USER} ${MONGO_PASSWORD}`;
+const MONGO_URL: string = `${MONGO_USER} ${MONGO_PASSWORD}`;
 
-mongoose.connect(url).then();
+//Redis config
+const REIDS_PORT: string = process.env.REDIS_PORT || '6379';
+const REDIS_URL: string = `redis://localhost:${REIDS_PORT}`;
+
+//Server config
+const SERVER_PORT: string = process.env.SERVER_PORT || '3000';
+
+export const config = {
+    mongo: {
+        url: MONGO_URL
+    },
+    redis: {
+        url: REDIS_URL
+    },
+    server: {
+        port: SERVER_PORT
+    }
+};
