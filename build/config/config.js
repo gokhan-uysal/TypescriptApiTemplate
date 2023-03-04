480e0a37ccb717f5532277d17e834047817462c9
Gökhan Uysal <guysal20@ku.edu.tr>
2023-03-04 14:13:43 +0300

    npm build and start commands added

---
+++ build/config/config.js
@@ -0,0 +1,28 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+//Mongo config
+const MONGO_USER = process.env.MONGO_USER || '';
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
+const MONGO_URL = `${MONGO_USER} ${MONGO_PASSWORD}`;
+//Redis config
+const REIDS_PORT = process.env.REDIS_PORT || '6379';
+const REDIS_URL = `redis://localhost:${REIDS_PORT}`;
+//Server config
+const SERVER_PORT = process.env.SERVER_PORT || '3000';
+exports.config = {
+    mongo: {
+        url: MONGO_URL
+    },
+    redis: {
+        url: REDIS_URL
+    },
+    server: {
+        port: SERVER_PORT
+    }
+};
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import redis from 'redis';
 import { config } from './config/config';
+
+const app = express();
+app.use(express.json());
+
+app.listen(config.server.port, () => {
+    console.log(`Server is listenning on port ${config.server.port}`);
+});
